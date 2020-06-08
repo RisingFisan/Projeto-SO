4c0b50c18b45398b463036cf5bf37e897e74733c
RisingFisan <sofiarsantos31@gmail.com>
2020-06-08 01:34:47 +0100

    Executar and listar commands should mostly work

--- src/server.c
+++ src/server.c
@@ -4,6 +4,8 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 void strcpyandtrim(char* dest, char* src);
 
@@ -11,8 +13,12 @@ int main(int argc, char const *argv[]) {
     mkfifo("./fifo", 0644);
     int fifo = open("./fifo", O_RDONLY);
     ssize_t bytesRead = 0;
+
+    char* inExecution[1024];
+    int lastProcess = 0;
+
     while(1) {
-        char buffer[1024];
+        char* buffer = calloc(1024, sizeof(char));
         int bytesRead = read(fifo, buffer, 1024);
         if(strncmp(buffer, "ajuda", 5) == 0) {
             char helpMessage[] = "tempo-inactividade TEMPO\n"
@@ -26,15 +32,78 @@ int main(int argc, char const *argv[]) {
             write(STDOUT_FILENO, helpMessage, strlen(helpMessage));
         }
         else if(strncmp(buffer, "executar", 8) == 0) {
-            char command[1024];
+            char* command = calloc(1024, sizeof(char));
             strcpyandtrim(command, buffer + 9);
+
+            inExecution[lastProcess] = strdup(command);
+            lastProcess++;
+
             write(STDOUT_FILENO, command, strlen(command));
             write(STDOUT_FILENO, "\n", 1);
+
+            int complexCommand = 0;
+            int pipes[1024][2];
+            int currPipe = 0;
+
+            char* token;
+            char* args[1000];
+            int i = 0;
+            while((token = strtok_r(command, " ", &command))) {
+                complexCommand = 1;
+                if(i == 0) {
+                    args[0] = strdup(token);
+                    i++;
+                }
+
+                if(*token == '|') {
+                    pipe(pipes[currPipe]);
+                    args[i] = NULL;
+                    if(fork() == 0) {
+                        close(pipes[currPipe][0]);
+                        if(currPipe != 0) {
+                            dup2(pipes[currPipe - 1][0], STDIN_FILENO);
+                            close(pipes[currPipe - 1][0]);
+                        }
+                        dup2(pipes[currPipe][1], STDOUT_FILENO);
+                        close(pipes[currPipe][1]);
+                        execvp(args[0], args + 1);
+                    }
+                    else {
+                        i = 0;
+                    }
+                    close(pipes[currPipe][1]);
+                    if(currPipe != 0) close(pipes[currPipe - 1][0]);
+                    currPipe++;
+                }
+                else {
+                    args[i] = strdup(token);
+                    i++;
+                }
+            }
+
+            args[i] = NULL;
+            if(fork() == 0) {
+                if(currPipe != 0) {
+                    dup2(pipes[currPipe - 1][0], STDIN_FILENO);
+                    close(pipes[currPipe - 1][0]);
+                }
+                execvp(args[0], args + 1);
+            }
         }
         else if(strncmp(buffer, "exit", 4) == 0) {
             break;
         }
+        else if(strncmp(buffer, "listar", 6) == 0) {
+            char message[1024];
+            for(size_t i = 0; i < lastProcess; i++) {
+                sprintf(message, "#%zu: %s", i + 1, inExecution[i]);
+                write(STDOUT_FILENO, message, strlen(message));
+                write(STDOUT_FILENO, "\n", 1);
+            }
+        }
         else write(STDOUT_FILENO, buffer, bytesRead);
+
+        free(buffer);
     }
     return 0;
 }
@@ -44,6 +113,6 @@ void strcpyandtrim(char* dest, char* src) {
     while(*start == ' ') start++;
     start++;
     strcpy(dest, start);
-    while(dest[strlen(dest) - 1] == ' ') dest[strlen(dest) - 1] = '\0';
+    while(dest[strlen(dest) - 1] != '"') dest[strlen(dest) - 1] = '\0';
     dest[strlen(dest) - 1] = '\0';
 }
